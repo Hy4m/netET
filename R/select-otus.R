615350b44432df8517f5005c44f3bf6c284fe0d7
houyun <houyunhuang@163.com>
2022-11-21 00:37:30 +0800

    more carefully handle reverse

--- R/select-otus.R
+++ R/select-otus.R
@@ -14,7 +14,7 @@ select_otus <- function(otu,
                         otu_on_rows = TRUE,
                         occur_thres = 0.5,
                         abund_thres = 0.001,
-                        reverse = NULL) {
+                        reverse = FALSE) {
   if (isTRUE(otu_on_rows)) {
     otu <- as.data.frame(t(otu))
   } else {
@@ -27,13 +27,34 @@ select_otus <- function(otu,
     stop("'otu' table is empty or all otus is zero.", call. = FALSE)
   }
 
+  if (isFALSE(reverse) || is.null(reverse)) {
+    reverse_occur <- reverse_abund <- FALSE
+  } else {
+    if (isTRUE(reverse)) {
+      reverse_occur <- reverse_abund <- TRUE
+    } else {
+      reverse <- tolower(reverse)
+      if (any(grepl("abund", reverse, fixed = TRUE))) {
+        reverse_abund <- TRUE
+      } else {
+        reverse_abund <- FALSE
+      }
+
+      if (any(grepl("occur", reverse, fixed = TRUE))) {
+        reverse_occur <- TRUE
+      } else {
+        reverse_occur <- FALSE
+      }
+    }
+  }
+
   if (!is.null(occur_thres)) {
     occur <- colSums(otu > 0, na.rm = TRUE)/nrow(otu)
 
-    if (!is.null(reverse) && any(grepl("occur", reverse, ignore.case = TRUE))) {
-      id_occur <- occur <= occur_thres
-    } else {
+    if (isFALSE(reverse_occur)) {
       id_occur <- occur >= occur_thres
+    } else {
+      id_occur <- occur <= occur_thres
     }
     otu <- otu[, id_occur, drop = FALSE]
   }
@@ -41,10 +62,10 @@ select_otus <- function(otu,
   if (!is.null(abund_thres)) {
     abund <- colSums(otu, na.rm = TRUE)/sum(otu, na.rm = TRUE)
 
-    if (!is.null(reverse) && any(grepl("abund", reverse, ignore.case = TRUE))) {
-      id_abund <- abund <= abund_thres
-    } else {
+    if (isFALSE(reverse_abund)) {
       id_abund <- abund >= abund_thres
+    } else {
+      id_abund <- abund <= abund_thres
     }
     otu <- otu[, id_abund, drop = FALSE]
   }
