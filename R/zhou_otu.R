27b04add9d7b2523d465eb78fd1dba1185bfa1ed
houyun <houyunhuang@163.com>
2022-12-01 23:12:06 +0800

    document data

---
+++ R/zhou_otu.R
@@ -0,0 +1,16 @@
+#' OTU table
+#'
+#' OTU table from Zhou(2022), see references for details.
+#'
+#' @format ## `zhou_otu`
+#' A data frame with 8816 rows and 60 columns:
+#' \describe{
+#'   row names of zhou_otu are OTU ID, and column names of zhou_otu are sample ID.
+#' }
+#' @docType data
+#' @references Zhou HL, Liu XY, Zhou C, Han SF, Xu FR, Dong X. Root-Associated
+#' Microbiomes of Panax notoginseng under the Combined Effect of Plant Development
+#' and Alpinia officinarum Hance Essential Oil. Molecules. 2022 Sep 15;27(18):6014.
+#' doi: 10.3390/molecules27186014. PMID: 36144749; PMCID: PMC9501277.
+#' @note Thanks to Dr. Zhou for providing and authorizing the original data.
+"zhou_otu"
---
+++ R/zhou_taxonomy.R
@@ -0,0 +1,22 @@
+#' Taxonomy table
+#'
+#' Taxonomy table from Zhou(2022), see references for details.
+#'
+#' @format ## `zhou_taxonomy`
+#' A data frame with 8816 rows and 7 columns:
+#' \describe{
+#'   \item{Kingdom}{Kingdom of root-associated microbiomes.}
+#'   \item{Phylum}{Phylum of root-associated microbiomes.}
+#'   \item{Class}{Class of root-associated microbiomes.}
+#'   \item{Order}{Order of root-associated microbiomes.}
+#'   \item{Family}{Family of root-associated microbiomes.}
+#'   \item{Genus}{Genus of root-associated microbiomes.}
+#'   \item{Species}{Species of root-associated microbiomes.}
+#' }
+#' @docType data
+#' @references Zhou HL, Liu XY, Zhou C, Han SF, Xu FR, Dong X. Root-Associated
+#' Microbiomes of Panax notoginseng under the Combined Effect of Plant Development
+#' and Alpinia officinarum Hance Essential Oil. Molecules. 2022 Sep 15;27(18):6014.
+#' doi: 10.3390/molecules27186014. PMID: 36144749; PMCID: PMC9501277.
+#' @note Thanks to Dr. Zhou for providing and authorizing the original data.
+"zhou_taxonomy"
